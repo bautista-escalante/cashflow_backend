6e9920b593b706684204b2e05d1ad98a3984d1cd
Your Name <you@example.com>
2026-04-21 09:17:52 -0300

    modo de prubas

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
+""" origins = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-]
+] """
 
 
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
